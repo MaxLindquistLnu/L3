3d7dd082b77dc0fce3f6590911e7a0076d3afffb
Max Lindquist <ml227cu@student.lnu.se>
2025-10-17 15:46:15 +0200

    add encode/decode option in dropdown component

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -14,6 +14,19 @@ function Dropdown(props: DropdownProps) {
     const { options, ...rest } = props;
     
     return (
+        <div className='dropdown-container'>
+        <div className="mydict">
+	        <div>
+		        <label>
+			        <input type="radio" name="radio"/>
+			        <span>Encode</span>
+		        </label>
+		        <label>
+			        <input type="radio" name="radio"/>
+			        <span>Decode</span>
+		        </label>
+	        </div>
+        </div>
         <select {...rest} className="dropdown">
             {options.map((option) => (
                 <option key={option.value} value={option.value}>
@@ -21,6 +34,7 @@ function Dropdown(props: DropdownProps) {
                 </option>
             ))}
         </select>
+        </div>
     );
 }
 
