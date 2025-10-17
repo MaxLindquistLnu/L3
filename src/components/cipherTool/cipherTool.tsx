ea96d4837b958b2249451b8cc23d99ebee508ba0
Max Lindquist <ml227cu@student.lnu.se>
2025-10-17 15:29:29 +0200

    implement dropdown menu in ciphertool

--- src/components/cipherTool/cipherTool.tsx
+++ src/components/cipherTool/cipherTool.tsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import InputField from './components/inputField/inputField'
 import OutputField from './components/outputField/outputField'
+import DropDown from '../dropdown/dropdown'
 import { Cipher } from '../../cipherModule/Cipher'
 import './cipherTool.css'
 
 function CipherTool() {
+  const [selectedCipher, setSelectedCipher] = useState('caesar')
   const [inputText, setInputText] = useState('')
   const [outputText, setOutputText] = useState('')
+
+  const cipherOptions = [
+        { value: 'caesar', label: 'Caesar Cipher' },
+        { value: 'vigenere', label: 'Vigenere Cipher' },
+        { value: 'atbash', label: 'Atbash Cipher' }
+  ]
 
   const handleInputChange = (value: string) => {
     setInputText(value)
@@ -20,6 +28,11 @@ function CipherTool() {
         value={inputText}
         onChange={(e) => handleInputChange(e.target.value)} 
         placeholder="Enter text"
+      />
+      <DropDown 
+        options={cipherOptions}
+        value={selectedCipher}
+        onChange={(e) => setSelectedCipher(e.target.value)}
       />
       <OutputField 
         value={outputText}
